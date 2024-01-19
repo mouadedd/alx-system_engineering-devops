5b148747e2d5743b983c91224fad3bae3cce7fd1
mouadedd <mdx.edd@gmail.com>
2024-01-19 04:11:51 -0800

    added pup

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,6 @@
+#!/usr/bin/pup
 # Ensure Python 3 and pip3 are installed
-package { 'python3':
-  ensure => installed,
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask using pip3 with the specified version
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => [Package['python3'], Package['python3-pip']],
 }
