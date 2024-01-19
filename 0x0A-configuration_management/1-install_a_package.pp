5403dc7f162f04667abdc7bc2829ba7bec01c248
mouadedd <mdx.edd@gmail.com>
2024-01-19 04:05:37 -0800

    added python package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
-# Using Puppet, install flask from pip3
+# Ensure Python 3 and pip3 are installed
+package { 'python3':
+  ensure => installed,
+}
 
-package {'flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask using pip3 with the specified version
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => [Package['python3'], Package['python3-pip']],
 }
