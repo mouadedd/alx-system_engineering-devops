4d469031113f2a4095f36836f21f5e90fe8cdb64
mouadedd <mdx.edd@gmail.com>
2024-02-19 05:12:25 -0800

    file created succefuly

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+use the url, for a given employee ID, returns information about his todos
+"""
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/"
+    id = argv[1]
+
+    user = requests.get(url + "{}".format(id)).json()
+    
+    todos = requests.get(url + "{}/todos".format(id)).json()
+
+    with open("{}.csv".format(id), "w", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [id, user.get("username"), elem.get("completed"), elem.get("title")]
+         ) for elem in todos]
