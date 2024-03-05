6caf420e964001b0a5554b8db29b289dce6ae94c
mouadedd <mdx.edd@gmail.com>
2024-03-05 03:41:00 -0800

    test

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """number of subscribers on a subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/smakosh)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    result = response.json().get("data")
+    return result.get("subscribers")
