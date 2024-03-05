4ab7194170918cf043e2b909cda2e4a7a463a3b1
mouadedd <mdx.edd@gmail.com>
2024-03-05 03:50:41 -0800

    not valid sub condition

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """number of subscribers on a subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': 'Python/requests:APIproject:\
     v1.0.0 (by /u/smakosh)'}).json()
