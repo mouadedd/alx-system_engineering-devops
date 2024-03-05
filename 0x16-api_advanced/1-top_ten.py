3a690e0ea6cbecc633c00d0bbbfc046610cbf30a
mouadedd <mdx.edd@gmail.com>
2024-03-05 04:53:18 -0800

    to test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,17 +5,16 @@ import requests
 
 def top_ten(subreddit):
     """10 hot posts on a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(elem.get("data").get("title")) for elem in results.get("children")]
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            art = dat.get("title")
+            print(art)
+    else:
+        print(None)
